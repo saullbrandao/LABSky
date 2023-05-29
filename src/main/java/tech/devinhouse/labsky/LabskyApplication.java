257fb940029ddf75d2b418849d879cdcdfd300f1
Saull Brandão <saull@outlook.com>
2023-05-29 20:23:38 -0300

    initial commit

---
+++ src/main/java/tech/devinhouse/labsky/LabskyApplication.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.labsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabskyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabskyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/devinhouse/labsky/LabskyApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.labsky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabskyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
